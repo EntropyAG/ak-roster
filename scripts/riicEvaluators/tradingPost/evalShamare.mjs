cc3a130d099e25ff676e21fcc69b9d31de6d7fff
Entropy <asmael.gaming@gmail.com>
2025-08-01 20:31:53 +0200

    Reworked Shamare PD contrib eval

--- scripts/riicEvaluators/tradingPost/evalShamare.mjs
+++ scripts/riicEvaluators/tradingPost/evalShamare.mjs
@@ -1,12 +1,23 @@
 import cnCharacterTable from "../../ArknightsGameData/zh_CN/gamedata/excel/character_table.json";
-
-import { tpOrders, tpDailyLmd } from "../../../src/data/riic/tpOrders.ts";
 import { roundTo } from "../../../src/util/fns/mathUtils.ts";
 
+import { tpOrders, tpDailyLmd } from "../../../src/data/riic/tpOrders.ts";
+
+// 2 (200%) = base TP PD (100%) + Shamare (90%) + CC buff (7%) + innate (3%)
+const SHAM_FULL_TP_PD = 2;
+// Shamare provides 2 * 45% productivity in a lvl 3 TP
+const SHAM_CONTRIB = 0.9;
+
+const MN_PER_DAY = 60 * 24;
+// 1 gold bar = 72mn to craft, so 60 * 24 / 72 = 20 gold bars crafted at default (100%) PD
+const BASELINE_FAC_GOLD_PER_DAY = 20;
+
 /**
- * Evaluate the player's roster to see whether it's viable to run PI/SR.
- * @param {Array[Operator]} operators: list of operators owned by the player, as imported
- * @param {Base} base: the base setup of the player
+ * Evaluate the player's roster to see whether it's viable to run a Shamare squad.
+ * The squad relies on Shamare for the raw productivity (90%), Tequila to improve the value
+ * of 4-bars orders and Tailoring alpha/beta to increase the frequency at which said 4-bars
+ * orders show up.
+ * @param {Roster} roster: all operators owned by the player.
  * @returns {Object}: with "rosmonPD" and "ebenPD" and their respective productivity
  * given the other operators
  */
@@ -19,6 +30,7 @@ const evalShamare = (roster) => {
         paprikaElite: -1,
         diamanteElite: -1,
         equivalentTpPd: 0,
+        equivalentFacPd: 0,
         alpha: 0,
         beta: 0
     };
@@ -63,7 +75,7 @@ const evalShamare = (roster) => {
         }
     }
 
-    // ============= Calculating PD =============
+    // ============= Calculating Trading post PD =============
     // Get the weighted value of LMD produced for 4-bars orders
     let weights = tpOrders.weights[1];
     if(results.beta >= 0){
@@ -78,16 +90,42 @@ const evalShamare = (roster) => {
     + weights[2] * barValueTequila * 4;
 
     // Get the weighted time
-    let squadPD = 1.93; // 90% due to Shamare + 3% from innate PD due to operators slotted
     let times = tpOrders.time;
     let weightedTime =
-        weights[0] * times[2] / squadPD
-      + weights[1] * times[3] / squadPD
-      + weights[2] * times[4] / squadPD;
+        weights[0] * times[2]
+      + weights[1] * times[3]
+      + weights[2] * times[4];
 
     // Divide both for estimated PD (TP3 as a baseline)
-    let lmdPerDay = weightedLMDValue / weightedTime * 24 * 60;
-    results.equivalentTpPd = roundTo((lmdPerDay / tpDailyLmd[2] - 1) * 100, 2);
+    let lmdPerDay = weightedLMDValue * MN_PER_DAY / weightedTime;
+    let pdGainOverBaseline = lmdPerDay / tpDailyLmd[2];
+    results.equivalentTpPd = roundTo(
+        (pdGainOverBaseline * SHAM_FULL_TP_PD - SHAM_FULL_TP_PD + SHAM_CONTRIB) * 100
+    , 2) 
+    ;
+
+    // ============= Calculating saved FAC PD =============
+    /**
+     * Explanation: Tequila increases the weighted average value of gold bars due to his base skill.
+     * In effect, if you are getting 10000 LMD a day from a TP, you'll need 20 gold bars by default,
+     * but with Tequila, you will get more LMD than that (say, 12000 for the sake of simplicity) while
+     * still needing only 20 gold bars. In effect, this means you don't need as much productivity from
+     * your FAC to keep up with an identical amount of LMD.
+     *
+     * This is especially important, because for a lvl 3 TP using a full E2 team with Shamare, Tequila
+     * and Tailoring Beta and assuming Amiya is in CC, you are saving about 46% productivity from a gold
+     * FAC. This is why the Shamare team almost always wins compared to others.
+     *
+     * You can check the following spreadsheet for the full calculations and use cases:
+     * https://docs.google.com/spreadsheets/d/10MLzrR2dTumC8edx2X6lN36L5n701jVeRpEqww89chM
+     */
+
+    // Tequila provides extra LMD value only on 4-bar orders, hence 2/3-bars orders can be ignored.
+    // The added value is either 250 LMD (0.5 bar) or 500 LMD (1 bar)
+    let weightedExtraBarsPerOrder = weights[2] * (tequila.elite === 2 ? 1 : 0.5);
+    let weightedGoldBonus = weightedExtraBarsPerOrder * MN_PER_DAY / weightedTime / BASELINE_FAC_GOLD_PER_DAY;
+    let goldContribution = weightedGoldBonus * SHAM_FULL_TP_PD;
+    results.equivalentFacPd = roundTo(goldContribution * 100, 2);
     return results;
 };
 
